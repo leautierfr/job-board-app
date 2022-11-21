48f0cc9da0d1c3ff62ef3ec4cdf5b9127ac3d5ef
bethpcassidy <116572434+bethpcassidy@users.noreply.github.com>
2022-11-21 16:27:39 -0500

    Revert "Company controller"

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-
-  get "/companies" => "companies#index"
-  get "/companies/:id" => "companies#show"
-  post "/companies" => "companies#create"
-
   get "/signup" => "users#new"
   post "/users" => "users#create"
 
@@ -12,5 +7,4 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
   get "/logout" => "sessions#destroy"
   resources :jobs
-
 end
