d4bbc377af2a69e132fb5e2f08a63e72f64ff848
beth <bethcassdy@gmail.com>
2022-11-21 14:42:30 -0500

    company model with table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<<<< Temporary merge branch 1
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_191519) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_193803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "logo"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.integer "company_id"
@@ -23,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_191519) do
     t.string "location"
     t.boolean "active"
     t.integer "salary_range"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_191519) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
