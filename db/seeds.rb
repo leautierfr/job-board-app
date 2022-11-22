8d52bc2e723d5467564b700d90234075f8cac0a7
Francois-Rene <leautier.fr@gmail.com>
2022-11-22 12:58:32 -0500

    fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-job = Job.create(company_id: 1, title: "Fisherman", description: "catches fish", url: "https://en.wikipedia.org/wiki/Fisherman", location: "Alaska", active: true, salary_range: "45000")
+Company.create(name: "Costco", logo: "https://1000logos.net/wp-content/uploads/2017/08/Font-Costco-Logo.jpg", description: "big-box retail store")
+Company.create(name: "Lidl", logo: "https://en.wikipedia.org/wiki/Lidl#/media/File:Lidl-Logo.svg", description: "discount retail store")
+Company.create(name: "McDonald's", logo: "https://en.wikipedia.org/wiki/McDonald%27s#/media/File:McDonald's_Golden_Arches.svg", description: "fast food restaurant")
+
+Job.create(company_id: 1, title: "cashier", description: "handle customer purchases", url: "https://en.wikipedia.org/wiki/Cashier", location: "Washington", active: "open", salary_range: 15000-20000)
+Job.create(company_id: 2, title: "manager", description: "assist and direct multiples employees", "url:https://en.wikipedia.org/wiki/Management", location: "New York", active: "open", salary_range: 15000-20000)
+Job.create(company_id: 2, title: "cook", description: "cook meals for customers", active: "open", salary_range: 20000-25000)
